4bbf6795945db7683fd1c03f16ec9d206be68ab0
정인호 <jeong-inho@jeong-inhoui-MacBookAir.local>
2022-12-31 14:38:52 +0900

    calculator모듈 test code 작성

---
+++ src/test/app.test.js
@@ -0,0 +1,5 @@
+const {add} = require('../app');
+
+test('', ()=>{
+    expect(add(1, 2)).toBe(3)
+}) 
---
+++ src/test/calculator.test.js
@@ -0,0 +1,60 @@
+const Calculator = require('../calculator');
+
+describe('Calculator', () => {
+    let cal;
+    beforeEach(() => {
+        cal = new Calculator();
+    });
+
+    it('inits with 0', () => {
+        expect(cal.value).toBe(0);
+    });
+
+    it('sets', () => {
+        cal.set(2);
+        expect(cal.value).toBe(2);
+    });
+
+    it('clear', () => {
+        cal.set(2);
+        cal.clear();
+        expect(cal.value).toBe(0);
+    })
+
+    it('adds', () => {
+        cal.set(1);
+        cal.add(2);
+        expect(cal.value).toBe(3);
+    })
+
+    it('subtracts', () => {
+        cal.set(5);
+        cal.subtract(2);
+        expect(cal.value).toBe(3);
+    })
+
+    it('multiplies', () => {
+        cal.set(5);
+        cal.multiply(2);
+        expect(cal.value).toBe(10);
+    })
+
+    describe('divides', () => {
+        it(' 0 / 0 === NaN', () => {
+            cal.divide(0);
+            expect(cal.value).toBe(NaN);
+        })
+
+        it('0 / 0 === Infity', () => {
+            cal.set(1);
+            cal.divide(0);
+            expect(cal.value).toBe(Infinity);
+        });
+
+        it('4 / 2 === 2', () => {
+            cal.set(4);
+            cal.divide(2);
+            expect(cal.value).toBe(2);
+        });
+    });
+});
