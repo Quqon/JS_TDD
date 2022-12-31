47151a3cb20c19bb4aff17430006b2b831d7bbae
정인호 <jeong-inho@jeong-inhoui-MacBookAir.local>
2022-12-31 14:19:58 +0900

    wip

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+const Calculator = require("./calculator");
+
 const add = (a, b) => {
     return a + b;
 }
+
+// let a = new Calculator();
+// a.add(3);
+// console.log(a.subtract(3));
+
 
 module.exports = {add};
