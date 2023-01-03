ca6d4f24532d9e986014319de34fec0f43cf5e80
정인호 <jeong-inho@jeong-inhoui-MacBookAir.local>
2023-01-03 13:01:39 +0900

    async test code

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -13,4 +13,17 @@ describe('fetchProduct', () => {
             expect(item).toEqual({ item: 'Milk', price: 200 });
         })
     })
+
+    it('async - await', async() => {
+        const product = await fetchProduct();
+        expect(product).toEqual({ item: 'Milk', price: 200 });
+    })
+
+    it('async - resolves', () => {
+        return expect(fetchProduct()).resolves.toEqual({ item: 'Milk', price: 200 });
+    })
+
+    it('async - reject', () => {
+        return expect(fetchProduct('error')).rejects.toBe('network error');
+    })
 })
